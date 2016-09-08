a79081ed060624b6cf65c9051334f308b850ce2a
gujiaweiqiu <m18020225795@163.com>
2016-09-08 19:58:09 +0800

    abd

---
+++ www/javaScript/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2016/9/8.
+ */
+$(document).ready(function(){
+    $("span").click(function(){
+            $("#sidebar").animate({
+                width:'toggle'
+            });
+
+    });
+});
+
